90ef40f331dcddb0be6f8dad7dbad8952149660b
anh.nguyen <kara_kim@icloud.com>
2018-09-17 17:19:37 +0700

    add lib

---
+++ lib/definitions/types/Command.d.ts
@@ -0,0 +1,14 @@
+import * as React from "react";
+import { EditorState } from "draft-js";
+export interface ButtonContentOptions {
+    iconProvider: (iconName: string) => React.ReactNode;
+}
+export declare type ButtonContentBuilder = (options: ButtonContentOptions) => React.ReactNode;
+export interface Command {
+    buttonComponentClass?: React.ComponentClass | string;
+    buttonContentBuilder: ButtonContentBuilder;
+    buttonProps?: any;
+    children?: Command[];
+    execute?: (EditorState: any, data?: any) => EditorState | Promise<EditorState>;
+}
+//# sourceMappingURL=Command.d.ts.map
